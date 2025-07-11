d21b8818ca6bfa4da5ea28a79e934c5c99820d60
Sneha Singh <sneha.singh@cloverinfotech.com>
2025-07-11 18:36:06 +0530

    Initial Commit

--- gym/gym_management_system/api.py
+++ gym/gym_management_system/api.py
@@ -1,75 +1,53 @@
 import frappe
-from frappe.auth import LoginManager
-from frappe import _
+from frappe.utils import nowdate, getdate
 
 @frappe.whitelist()
-def authenticate_user(email, password, role):
-    """Authenticate the user using email, password, and role."""
-    
-   
-    gym_member = frappe.get_all(
-        "Gym Members", 
-        filters={"email": email}, 
-        fields=["name1", "role", "password"] 
+def get_member_details():
+    user_email = frappe.session.user
+
+
+    gym_member_name = frappe.get_value("Gym Members", {"email": user_email}, "name")
+    if not gym_member_name:
+        return {}
+
+ 
+    membership = frappe.get_all(
+        'Gym Membership',
+        filters={'email_id': gym_member_name},
+        limit=1,
+        order_by='creation desc',
+        fields=['email_id', 'contact', 'plans', 'joining_date', 'ending_date']
     )
 
-    if not gym_member:
-        return {"success": False, "message": "Invalid email"}  
+    if not membership:
+        return {}
 
-    
-    stored_password = gym_member[0]["password"]
-    
-    
-    if stored_password != password:
-        return {"success": False, "message": "Invalid password"}  
-    
+    member = membership[0]
 
-    if gym_member[0]["role"] != role:
-        return {"success": False, "message": "Invalid role"}  
-
-   
-    return {"success": True, "message": "Authentication successful", "role": gym_member[0]["role"]}
-
-
-import frappe
-from datetime import datetime
-
-@frappe.whitelist(allow_guest=True)
-def get_member_details(email):
+ 
+    remaining_days = None
+    if member.get('ending_date'):
+        remaining_days = (getdate(member['ending_date']) - getdate(nowdate())).days
 
   
-    membership = frappe.get_doc("Gym Membership", email)
+    booking = frappe.get_all(
+        'Gym Class Booking',
+        filters={'email': user_email},
+        limit=1,
+        order_by='creation desc',
+        fields=['trainer_name']
+    )
 
-  
-    today = datetime.today().date()
-    ending_date = membership.ending_date
-    remaining_days = (ending_date - today).days if ending_date else None
-
-   
-    booking = frappe.get_all("Gym Class Booking",
-                             filters={"email": email},
-                             fields=["trainer_name"],
-                             limit_page_length=1)
-
-    trainer_name = booking[0].trainer_name if booking else None
-    trainer_info = {}
-    
-    if trainer_name:
-        trainer = frappe.get_doc("TrainerReg", trainer_name)
-        trainer_info = {
-            "name": trainer.name,
-            "email_id": trainer.email_id,
-            "contact_no": trainer.contact_no,
-            "address": trainer.address,
-            "gender": trainer.gender
-        }
+    trainer_name = booking[0]['trainer_name'] if booking else 'N/A'
 
     return {
-        "email": membership.email_id,
-        "contact": membership.contact,
-        "plan": membership.plans,
-        "joining_date": membership.joining_date,
-        "ending_date": membership.ending_date,
+        "email": user_email,
+        "contact": member.get('contact'),
+        "plan": member.get('plans'),
+        "joining_date": member.get('joining_date'),
+        "ending_date": member.get('ending_date'),
         "remaining_days": remaining_days,
-        "trainer": trainer_info
+        "trainer": {
+            "name": trainer_name
+        }
     }
--- gym/gym_management_system/doctype/gym_members/gym_members.py
+++ gym/gym_management_system/doctype/gym_members/gym_members.py
@@ -1,9 +1,39 @@
-# Copyright (c) 2025, Sneha and contributors
+# Copyright (c) 2025, Sneha
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class GymMembers(Document):
-	pass
+    def after_insert(self):
+        self.create_user()
+
+    def create_user(self):
+        if not self.email:
+            frappe.throw("Email is required to create user.")
+
+     
+        if frappe.db.exists("User", self.email):
+            frappe.msgprint(f"User {self.email} already exists.")
+            return
+
+        
+        user_type = "System User" if self.role == "Gym Admin" else "Website User"
+
+        
+        user = frappe.get_doc({
+            "doctype": "User",
+            "email": self.email,
+            "first_name": self.name1,  
+            "user_type": user_type,
+            "send_welcome_email": 1,
+            "new_password": self.password  
+        })
+
+        
+        if self.role:
+            user.append("roles", {"role": self.role})
+
+      
+        user.insert(ignore_permissions=True)
+        frappe.msgprint(f"User {self.email} created with role {self.role}.")
